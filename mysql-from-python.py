ed2e7a0983ab975ea693b651f9a0654991bb5e6d
Joe <joebreilly@yahoo.ie>
2021-07-17 14:53:17 +0000

    sql queries with python added to mysql-from-python.py

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -13,12 +13,11 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['Jim', 'Bob']
+        format_strings = ",".join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});"
+                       .format(format_strings), list_of_names)
+        connection.commit()
 finally:
-    # Close the connection, regardless of whether or not the above was successful
     connection.close()
